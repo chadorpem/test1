654094fdb74d8f7a941652732b9b392b6a1d1c9d
chador02230093 <02230093.cst@rub.edu.bt>
2024-02-26 14:54:44 +0600

    second commit logical.py

---
+++ logical.py
@@ -0,0 +1,23 @@
+#example 1
+x=5
+print(x>3 and x<10)#true becoz both are true
+
+#example 2
+y=12
+print(y>10 and y%5==0) #false becoz the second sentence is false
+
+#example 1
+z=5
+print(z<3 or z>10) #false becoz conditional are false
+
+#example 2
+a=12
+print(a<10 or a%2==0)#true becoz the second statement is true
+
+#example 1
+b=5
+print(not(b>3 and b<10))#false becoz the condition iside is not true
+
+#example 2
+c=12
+print(not(c>10 and c%5==0))#true becoz the condition inside is not is false
