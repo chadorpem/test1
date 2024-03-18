48ef80f9e6439ceb8bfcb25ee02405f88b433f88
chador02230093 <02230093.cst@rub.edu.bt>
2024-03-18 15:07:12 +0600

    Adding student_grade.py file

---
+++ student_grade.py
@@ -0,0 +1,11 @@
+num_students=int(input('Enter the number of students:'))
+i=1
+while i <=num_students:
+    total_grade=0
+    num_subject=int(input('Enter the number of subjects for studentss {i}:'))
+for j in range(1,num_subject+1):
+    grade=float(input(f'Enter subject{j} grade for student{i}:'))
+    total_grade+=grade
+    average_grade=total_grade/num_subject
+    print(f'average grade for student{i}: {average_grade:.2f}')
+    i+=1
