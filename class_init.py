f49d5bc08c8406ba4a02e616f34899196e55f6ae
chador02230093 <02230093.cst@rub.edu.bt>
2024-05-13 20:35:40 +0600

    adding class_init.py file

---
+++ class_init.py
@@ -0,0 +1,18 @@
+class dog:
+    def __init__(self,name,colour,age):
+        self.name=name
+        self.colour=colour
+        self.age=age
+        
+dawa=dog('dawa','blue',2)
+nima=dog('nima','black',1)
+blacky=dog('blacky','yellow',3)
+print(dawa.name)
+print(dawa.colour)
+print(dawa.age)
+print(nima.name)
+print(nima.colour)
+print(nima.age)
+print(blacky.name)
+print(blacky.colour)
+print(blacky.age)
