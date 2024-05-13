83b19718dae1e1705174af9719d7c6bec1c21afa
chador02230093 <02230093.cst@rub.edu.bt>
2024-05-13 20:37:31 +0600

    adding classwork.py file

---
+++ classwork.py
@@ -0,0 +1,12 @@
+name=input('Enter your name:')
+num1=float(input('enter any number:'))
+num2=float(input('Enter any one number:'))
+addition=num1+num2
+addition_result=print(addition)
+subtraction=num1-num2
+subtraction_result=print(subtraction)
+multiply=num1*num2
+multiplication_result=print(multiply)
+division=num1/num2
+division_result=print(division)
+print(f'hello {name}')
